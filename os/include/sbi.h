84cf32158bf3147e51b6a4b01ccdc8f865e9b1bd
build <build@deb.deb>
2026-01-14 20:04:08 +0800

    feat:exercises

--- os/include/sbi.h
+++ os/include/sbi.h
@@ -3,4 +3,29 @@
 
 #include "types.h"
 
+// SBI 扩展ID
+#define SBI_EXT_0_1_CONSOLE_PUTCHAR 0x1
+#define SBI_EXT_BASE 0x10
+
+// SBI 函数ID
+#define SBI_CONSOLE_PUTCHAR 0
+#define SBI_CONSOLE_GETCHAR 1
+
+struct sbiret {
+    long error;
+    long value;
+};
+
+
+// SBI 调用封装
+static inline struct sbiret sbi_ecall(int ext, int fid, uint64_t arg0, 
+                                      uint64_t arg1, uint64_t arg2,
+                                      uint64_t arg3, uint64_t arg4,
+                                      uint64_t arg5) {
+    struct sbiret ret;
+}
+
+
+
+
 #endif
