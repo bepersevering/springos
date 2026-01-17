5ae03f85d1637b1fba06b51825734f6eb7d77d4e
bepersevering <mingshashan@gmail.com>
2026-01-17 22:04:14 +0800

    feat: exercise

--- os/include/sbi.h
+++ os/include/sbi.h
@@ -23,7 +23,31 @@ static inline struct sbiret sbi_ecall(int ext, int fid, uint64_t arg0,
                                       uint64_t arg3, uint64_t arg4,
                                       uint64_t arg5) {
     struct sbiret ret;
+    register uint64_t a0 asm ("a0") = (uint64_t)(arg0);
+    register uint64_t a1 asm ("a1") = (uint64_t)(arg1);
+    register uint64_t a2 asm ("a2") = (uint64_t)(arg2);
+    register uint64_t a3 asm ("a3") = (uint64_t)(arg3);
+    register uint64_t a4 asm ("a4") = (uint64_t)(arg4);
+    register uint64_t a5 asm ("a5") = (uint64_t)(arg5);
+    register uint64_t a6 asm ("a6") = (uint64_t)(fid);
+    register uint64_t a7 asm ("a7") = (uint64_t)(ext);
+
+    asm volatile("ecall"
+                : "=r" (a0), "=r" (a1)
+                : "r" (a0), "r" (a1), "r" (a2), "r" (a3),
+                 "r" (a4), "r" (a5), "r" (a6), "r" (a7)
+                : "memory");
+    ret.error = a0;
+    ret.value = a1;
+    return ret;
 }
+
+
+// SBI 控制台输出字符
+static inline void sbi_console_putchar(int ch) {
+    sbi_ecall(SBI_EXT_0_1_CONSOLE_PUTCHAR, SBI_CONSOLE_PUTCHAR, ch, 0, 0, 0, 0, 0);
+}
+
 
 
 
